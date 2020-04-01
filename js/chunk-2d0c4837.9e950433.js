9d5634d512503723fce181b1b99800b941bcfe6e
Diego Rodrigo <diegorodrigo90@gmail.com>
2020-04-01 12:07:59 -0300

    prismic

---
+++ js/chunk-2d0c4837.9e950433.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c4837"],{"3ac6":function(t,e,s){"use strict";s.r(e);var c=function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"post-part single container"},[s("prismic-rich-text",{staticClass:"textslice",attrs:{field:t.slice.primary.text}})],1)},i=[],n={props:["slice"],name:"text-slice"},a=n,l=s("2877"),r=Object(l["a"])(a,c,i,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0c4837.9e950433.js.map
